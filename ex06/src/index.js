b3abdaec4a13e62ca33be155b62569318eaf5c35
aharis <hare.arapovic@gmail.com>
2021-03-13 23:22:02 +0100

    dodao index.js file

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    
+    // Only change code below this line
+    var  myName ="Haris";
+    var myString ="My name is " + myName + " and I am well!";
+    
+    // Onliy change code above this line
+
+    return myString;
+}
+
+console.log (main());
+module.exports = main;
