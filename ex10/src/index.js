5a5895ec7c5388380b637c42771369dc8452f0dc
aharis <hare.arapovic@gmail.com>
2021-03-13 23:26:55 +0100

    dodao index.js file

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+
+    var  myString = "Jellow World";
+         myString= "Hello World";
+    
+    // Onliy change code above this line
+
+    return myString;
+}
+
+console.log (main());
+module.exports = main;
