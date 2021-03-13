b79aa49f42a0b9cbf04415df0ee59133a31526ee
aharis <hare.arapovic@gmail.com>
2021-03-13 23:20:48 +0100

    dodao index.js file

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+    var  myString ="Concatenation is the process of appending ";
+        myString +="one string to the and of another string.";
+    
+    // Onliy change code above this line
+
+    return myString;
+}
+
+console.log (main());
+module.exports = main;
