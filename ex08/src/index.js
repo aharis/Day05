04378f67844f1aac90562ccb24e8672118c49997
aharis <hare.arapovic@gmail.com>
2021-03-13 23:24:58 +0100

    dodao index.js file

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+
+    var  lastName = "Doe";
+    var lastNameLength = lastName.length;
+    
+    // Onliy change code above this line
+
+    return lastNameLength;
+}
+
+console.log (main());
+module.exports = main;
