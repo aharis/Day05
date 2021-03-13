de5b54a2188b5791fb7b907bb2472cf5d70448bd
aharis <hare.arapovic@gmail.com>
2021-03-13 23:33:45 +0100

    dodao index.js file

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    // Only change code below this line
+    var myNoun ="dog";
+    var myVerb ="run";
+    var myAdjective ="big";
+    var myAdverb ="quickly";
+    var wordBlanks =  myAdjective +" "+ myNoun +" "+ myVerb+" "+ myAdverb;
+   
+    
+    // Onliy change code above this line
+
+    return wordBlanks;
+}
+
+console.log (main());
+module.exports = main;
