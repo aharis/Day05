8402fb7116205551028c99e170dfe3522feaad7d
aharis <hare.arapovic@gmail.com>
2021-03-13 23:23:50 +0100

    dodao index.js file

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    var  someAdjecitive ="the best!";
+    var myString ="Mistral is ";
+    myString += someAdjecitive;
+    
+    // Only change code above this line
+
+    return myString;
+}
+
+console.log (main());
+module.exports = main;
