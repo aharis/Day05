407d5d8d8e19e3c54d4baf73d31f1313b187b4da
aharis <hare.arapovic@gmail.com>
2021-03-13 23:28:37 +0100

    dodao index.js file

---
+++ ex11/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+
+    var  firstName ="Tesla";
+    var secondLetterOfFirstName = firstName[2];
+    
+    // Onliy change code above this line
+
+    return secondLetterOfFirstName;
+}
+
+console.log (main());
+module.exports = main;
